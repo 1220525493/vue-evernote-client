36017d4291657c5e0b9a52b44798942794ebe90a
alex <1220525493@qq.com>
2022-03-01 16:29:01 +0800

    我的项目

---
+++ src/components/NotebookList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="notebook-list">
+    <h1>{{msg}}</h1>
+    <ul>
+      <li><router-link to ="/note/1"></router-link></li>
+      <li><router-link to ="/note/2"></router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      msg:'笔记本列表'
+    }
+  }
+}
+
+
+</script>
+<style scoped>
+h1{
+  color: red;
+}
+</style>
+
---
+++ src/components/TrashDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="trash-detail">
+    <h1>{{msg}}:{{$route.params.noteId}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'login',
+  data(){
+    return{
+      msg:'回收站笔记详情页'
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+h1{
+  color: blue;
+}
+
+</style>
